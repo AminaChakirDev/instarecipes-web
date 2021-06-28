5fcf14c5319c935264f963f55becaf8eaa17eb0d
Amina <amina.aitm@gmail.com>
2021-06-28 16:02:03 +0200

    Create Accessory Component

---
+++ src/components/AccessoriesList.js
@@ -0,0 +1,26 @@
+import { useQuery, gql } from '@apollo/client';
+import Accessory from "./Accessory";
+
+const ACCESSORIES = gql`
+query getAccessories {
+  getAccessories {
+    _id
+    title
+    icon
+  }
+}
+`;
+
+function AccessoriesList() {
+
+    const { loading, error, data } = useQuery(ACCESSORIES);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+
+    return data.getAccessories.map(({ title, icon }) =>(
+        <Accessory title={title} icon={icon}/>
+    ));
+}
+
+export default AccessoriesList;
