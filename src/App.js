8275ca07a1bb29fb648caa6de1f8952472361a66
Amina <amina.aitm@gmail.com>
2021-06-29 14:11:04 +0200

    Add logo and favorite icon

--- src/App.js
+++ src/App.js
@@ -11,37 +11,58 @@ import AccessoriesList from "./components/AccessoriesList";
 import CategoriesList from "./components/CategoriesList";
 import RecipesList from "./components/RecipesList";
 import Admin from "./components/Admin";
+import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from '@material-ui/icons/Search';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import Search from "./components/Search";
+import Favorites from "./components/Favorites";
+import logo from './logo.png';
 
 function App() {
   return (
     <div className="App">
         <Router>
             <div>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/ingredients">Ingredients</Link>
-                    </li>
-                    <li>
-                        <Link to="/accessories">Accessories</Link>
-                    </li>
-                    <li>
-                        <Link to="/categories">Categories</Link>
-                    </li>
-                    <li>
-                        <Link to="/recipes">Recipes</Link>
-                    </li>
-                    <li>
-                        <Link to="/admin">Admin</Link>
-                    </li>
-                </ul>
-
-                <hr />
+                <div className="header">
+                    <div>
+                        <Link to="/"><img src={logo} alt="Logo" className="logo" /></Link>
+                    </div>
+                    <ul className="menu-container">
+                        <li>
+                            <Link to="/"><HomeIcon fontSize="large"/></Link>
+                        </li>
+                        <li>
+                            <Link to="/search"><SearchIcon fontSize="large"/></Link>
+                        </li>
+                        <li>
+                            <Link to="/favorites"><FavoriteIcon fontSize="large"/></Link>
+                        </li>
+                        <li>
+                            <Link to="/ingredients">Ingredients</Link>
+                        </li>
+                        <li>
+                            <Link to="/accessories">Accessories</Link>
+                        </li>
+                        <li>
+                            <Link to="/categories">Categories</Link>
+                        </li>
+                        <li>
+                            <Link to="/recipes">Recipes</Link>
+                        </li>
+                        <li>
+                            <Link to="/admin">Admin</Link>
+                        </li>
+                    </ul>
+                </div>
                 <Switch>
                     <Route exact path="/">
                         <Home />
+                    </Route>
+                    <Route path="/search">
+                        <Search />
+                    </Route>
+                    <Route path="/favorites">
+                        <Favorites />
                     </Route>
                     <Route path="/ingredients">
                         <IngredientsList />
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,20 +1,32 @@
 import InstagramIcon from '@material-ui/icons/Instagram';
 import RestaurantIcon from '@material-ui/icons/Restaurant';
 import AvTimerIcon from '@material-ui/icons/AvTimer';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import {Image} from 'cloudinary-react';
+import {useState} from "react";
 
 function Recipe({instagramAuthor, preparationTime, title, poster, categories}) {
+
+    const [favoriteSate, setFavoriteState] = useState(false);
+
+    const handleClick = () => {
+        setFavoriteState(!favoriteSate);
+    }
 
     return (
         <div className="recipe-main-container" key={title}>
             <div className="recipe-poster-container">
                 <Image className="recipe-poster" cloudName="dz632zpoz" publicId={poster} width="100" crop="scale" />
             </div>
-            <div className="recipe-details-container">
-                <div className="recipe-detail-container recipe-title">{title}</div>
-                <div className="recipe-detail-container"><InstagramIcon/><span className="recipe-detail">@{instagramAuthor}</span></div>
-                <div className="recipe-detail-container"><RestaurantIcon/><span className="recipe-detail">{categories[0].title}</span></div>
-                <div className="recipe-detail-container"><AvTimerIcon/><span className="recipe-detail">{preparationTime}</span></div>
+            <div className="recipe-bottom">
+                <div className="recipe-details-container">
+                    <div className="recipe-detail-container recipe-title">{title}</div>
+                    <div className="recipe-detail-container"><InstagramIcon/><span className="recipe-detail">@{instagramAuthor}</span></div>
+                    <div className="recipe-detail-container"><RestaurantIcon/><span className="recipe-detail">{categories[0].title}</span></div>
+                    <div className="recipe-detail-container"><AvTimerIcon/><span className="recipe-detail">{preparationTime}</span></div>
+                </div>
+                <div className="recipe-favorite-icon" onClick={()=>handleClick()}>{favoriteSate ? <FavoriteIcon fontSize="large"/> : <FavoriteBorderIcon fontSize="large"/>}</div>
             </div>
         </div>
     )
