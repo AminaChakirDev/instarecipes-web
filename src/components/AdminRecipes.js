5bf15e48cde2527cef517f965f5a2a0845cc97c4
Amina <amina.aitm@gmail.com>
2021-06-30 10:42:28 +0200

    Style create recipe form

--- src/components/AdminRecipes.js
+++ src/components/AdminRecipes.js
@@ -1,21 +1,10 @@
 import CreateRecipe from "./CreateRecipe";
-import UploadPoster from "./UploadPoster";
 import DeleteRecipe from "./DeleteRecipe";
-import {Link} from "react-router-dom";
-import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 
 function AdminRecipes() {
     return (
         <div className="admin-page">
             <h2>Gestion des recettes</h2>
-
-            <Link to="/admin">
-                <div className="back-button-container">
-                    <ArrowBackIcon/>
-                    <span>Retour vers la page admin</span>
-                </div>
-            </Link>
-
             <CreateRecipe/>
             <DeleteRecipe/>
         </div>
